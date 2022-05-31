fe8e0d6ee8ce378a7057210a7aca02b1b875f3f1
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-05-30 21:06:30 -0300

    criacao de arquivo para utilizar modules, toda parte de selecao de niveis de dificuldade e exibicao foi colocada dentro do arquivo

---
+++ modules/dificulty.js
@@ -0,0 +1,32 @@
+const boxDificulty = document.getElementById('dificulty');
+const box = document.getElementById('box');
+const submit = document.getElementById('card-restart');
+
+let [easyPage, mediumPage, hardPage] = [false, false, false]
+
+function easy() {
+    easyPage = true;
+    const el = document.getElementById('game-easy');
+    showGame(el);
+}
+
+function medium() {
+    mediumPage = true;
+    const el = document.getElementById('game-medium');
+    showGame(el);
+}
+
+function hard() {
+    hardPage = true
+    const el = document.getElementById('game-hard');
+    showGame(el)
+}
+
+function showGame(el) {
+    el.style.display = 'flex';
+    submit.style.display = 'flex';
+    boxDificulty.style.display = 'none';
+    box.style.display = 'none';
+}
+
+export { easy, medium, hard, easyPage, mediumPage, hardPage }
