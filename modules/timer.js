2f0ec7ba139ffa91077d1ef192ac71cf57fd9268
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-08-15 20:26:27 -0300

    remocao de variaveis de hora e milisegundo

--- modules/timer.js
+++ modules/timer.js
@@ -1,4 +1,3 @@
-let hour = 0;
 let minutes = 0;
 let seconds = 0;
 let milliseconds = 0;
@@ -7,22 +6,16 @@ let stopwatch;
 function startTimer() {
   stopTimer();
   reset();
-  stopwatch = setInterval(() => timer(), 10)
+  stopwatch = setInterval(() => timer(), 1000)
 }
 
 function timer() {
-  if ((milliseconds += 10) === 1000) {
-    milliseconds = 0;
-    seconds++
-  };
+  seconds ++;
+  
   if (seconds === 60) {
     seconds = 0;
     minutes++
   };
-  if (hour === 60) {
-    minutes = 0;
-    hour++
-  }
 
   showTimer();
 }
@@ -32,25 +25,20 @@ function stopTimer() {
 }
 
 function reset() {
-  hour = 0;
   minutes = 0;
   seconds = 0;
   milliseconds = 0;
-  document.getElementById('hour').innerText = '00';
     document.getElementById('minute').innerText = '00';
     document.getElementById('second').innerText = '00';
-    document.getElementById('millisecond').innerText = '00';
 }
 
 function showTimer() {
-  document.getElementById('hour').innerText = formatTime(hour)
   document.getElementById('minute').innerText = formatTime(minutes)
   document.getElementById('second').innerText = formatTime(seconds)
-  document.getElementById('millisecond').innerText = formatTime(milliseconds)
 }
 
 function formatTime(num) {
-  return num > 10 ? num : `0${num}`
+  return num >= 10 ? num : `0${num}`
 }
 
 export {startTimer, stopTimer}
